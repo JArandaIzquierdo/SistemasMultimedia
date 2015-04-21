b02db6d7d404b711c2917cc9e81f051a3b68c66b
Javi Aranda <j.arandaizquierdo@gmail.com>
2015-04-21 12:44:09 +0200

    Agregados los metodos a la clase abstracta y extendida la clase Linea de la clase abstracta tambien

--- SM.JAI.Biblioteca/src/sm/JAI/graficos/AbstracLineaPunto.java
+++ SM.JAI.Biblioteca/src/sm/JAI/graficos/AbstracLineaPunto.java
@@ -1,12 +1,56 @@
 package sm.JAI.graficos;
 
 import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 /**
  *
  * @author Javier Aranda
  */
 public abstract class AbstracLineaPunto extends Line2D{
+
+    @Override
+    public double getX1() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getY1() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Point2D getP1() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getX2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getY2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Point2D getP2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setLine(double x1, double y1, double x2, double y2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Rectangle2D getBounds2D() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
-    
+    public boolean isNear(Point2D p){
+        return this.ptLineDist(p)<=2.0;
+    }
 }
--- SM.JAI.Biblioteca/src/sm/JAI/graficos/Linea.java
+++ SM.JAI.Biblioteca/src/sm/JAI/graficos/Linea.java
@@ -1,11 +1,10 @@
 package sm.JAI.graficos;
 
-import java.awt.geom.Line2D;
 
 /**
  *
  * @author Javier Aranda
  */
-public class Linea extends Line2D {
+public class Linea extends AbstracLineaPunto {
     
 }
